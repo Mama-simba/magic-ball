ff48afb45d827b55ad4439a766e4139ffa68f2f8
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2021-05-03 09:57:00 +0200

    HTML, CSS & JS files created

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+var phrases = [
+    "Yes!",
+    "No!",
+    "It will pass",
+    "Count on it",
+    "Maybe",
+    "You're hot",
+    "Ask again",
+    "No doubt",
+    "Absolutely",
+    "Go for it",
+    "Wait for it",
+    "Not now",
+    "Cannot tell now",
+    "Very likely",
+    "It is ok"
+    ]
