5707012a7bac1379f2f6f4c1bdcfe46e8b950c9d
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2021-05-03 11:44:07 +0200

    JS interaction added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
+function showMessage(event) {
+           var answer = [
+               "Yes!",
+               "No!",
+               "Not today",
+               "Count on it",
+               "Maybe",
+               "That sounds good!",
+               "Ask again",
+               "No doubt",
+               "Absolutely",
+               "Go for it",
+               "Wait for it",
+               "Not now",
+               "Cannot tell now",
+               "Very likely",
+               "It is ok",
+               "Very doubtful",
+               "You may rely on it",
+               "Try again",
+               "Outlook not so good"
+           ]
 
+           
+            let result = document.querySelector("#result");
+            let randomAnswer = answer[Math.floor(Math.random() * 18)];
+            return result.innerHTML = randomAnswer;
+        }
 
-
-
-
-var phrases = [
-    "Yes!",
-    "No!",
-    "It will pass",
-    "Count on it",
-    "Maybe",
-    "You're hot",
-    "Ask again",
-    "No doubt",
-    "Absolutely",
-    "Go for it",
-    "Wait for it",
-    "Not now",
-    "Cannot tell now",
-    "Very likely",
-    "It is ok"
-    ]
+        let sendButton = document.querySelector("button");
+        sendButton.addEventListener("click", showMessage);
